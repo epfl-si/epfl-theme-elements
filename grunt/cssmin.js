3d3d3f0f483f024b0722ce89d56c342c1f2aa8c7
Olivier Bieler <olivier.bieler@epfl.ch>
2019-11-13 14:11:04 +0100

    add vendors css

--- grunt/cssmin.js
+++ grunt/cssmin.js
@@ -10,6 +10,7 @@ module.exports = {
     files: {
       'tmp/css/css-elements-for-cloudflare-custom-pages.min.css': [
         'tmp/css/base.css',
+        'tmp/css/vendors.min.css',
         'src/css/utilities.css'
       ]
     }
